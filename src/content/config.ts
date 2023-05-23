a440e3fdb97a7b31172eb56f2d7bfc129c9f94c8
Michaël Gainyo <michaelgainyo@gmail.com>
2023-05-23 23:19:13 +0100

    base structure + style

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+    publish: z.boolean().optional(),
+  }),
 });
 
 export const collections = { blog };
