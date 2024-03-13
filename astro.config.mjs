68529d0669b7be770c72594e32e18d1924e17e59
Michaël Gainyo <michaelgainyo@gmail.com>
2024-03-13 23:51:06 +0100

    fix: rehype error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,37 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
-import rehypePrettyCode from 'rehype-pretty-code';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://michaelgainyo.github.io',
+  site: "https://michaelgainyo.github.io",
   integrations: [mdx(), UnoCSS(), sitemap()],
   server: { port: 3010, host: true },
   markdown: {
-    shikiConfig: { theme: '' },
+    shikiConfig: { theme: "" },
     extendDefaultPlugins: true,
     syntaxHighlight: false,
     rehypePlugins: [
       [
         rehypePrettyCode,
         {
-          theme: 'material-theme-palenight',
+          theme: "material-theme-palenight",
           onVisitLine(node) {
-            if (node.children.length === 0) node.children = [{ type: 'text', value: ' ' }];
+            if (node.children.length === 0) node.children = [{ type: "text", value: " " }];
           },
           onVisitHighlightedLine(node) {
-            node.properties.className.push('highlighted');
+            const nodeClass = node.properties.className;
+            console.log("Highlighted Line", { node });
+            if (nodeClass && nodeClass.length > 0) {
+              node.properties.className.push("highlighted");
+            } else {
+              node.properties.className = ["highlighted"];
+            }
           },
           onVisitHighlightedWord(node) {
-            node.properties.className = ['word'];
+            node.properties.className = ["word"];
           },
           tokensMap: {},
         },
